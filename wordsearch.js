7e0dcf1b5501c6ae403dffaab5dbd7ed85e2f2d2
Bryce McLachlan <brycemclachlan@gmail.com>
2021-01-14 03:57:27 +0000

    Completed pair programing challange with Jacqueline Smith & Molly Trepanier

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,46 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+    //horizontal
+    let tf = false;
+    letters.forEach((item) => {
+        if (item.join("").includes(word)) {
+            tf = true;
+        }
+    });
+    //col
+    transpose(letters).forEach((item) => {
+        if (item.join("").includes(word)) {
+            tf = true;
+        }
+    });
+    //reverse horizontal
+    letters.forEach((item) => {
+        if (item.reverse().join("").includes(word)) {
+            tf = true;
+        }
+    });
+    //reverse col
+    transpose(letters).forEach((item) => {
+        if (item.reverse().join("").includes(word)) {
+            tf = true;
+        }
+    });
 
-module.exports = wordSearch
+    return tf;
+};
+
+const transpose = function (matrix) {
+    let finalMatrix = [];
+    if (matrix.length === 0) {
+        return [];
+    }
+    for (let row = 0; row < matrix.length; row++) {
+        for (let col = 0; col < matrix[row].length; col++) {
+            if (!finalMatrix[col]) {
+                finalMatrix[col] = [];
+            }
+            finalMatrix[col].push(matrix[row][col]);
+        }
+    }
+    return finalMatrix;
+};
+module.exports = wordSearch;
